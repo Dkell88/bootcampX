a1b069eacd632c013d5f499f352ddbbf2181a2a7
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-02 22:13:04 -0700

    Add 8 queries to 4_queries

---
+++ 4_queries/4_average_cohort_assistance_time.sql
@@ -0,0 +1,13 @@
+-- SELECT cohorts.name as name, AVG(assistance_requests.completed_at - assistance_requests.started_at) as average_time
+-- FROM assistance_requests
+-- JOIN students ON students.id = student_id
+-- JOIN cohorts ON student_id = students.id
+-- GROUP BY cohorts.name
+-- ORDER BY AVG(assistance_requests.completed_at - assistance_requests.started_at);
+
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
