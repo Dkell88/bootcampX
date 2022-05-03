6b17d28ee9c6b26b060c8c650bb31332fa8faf14
Dkell88 <DarrenKelly1188@gmail.com>
2022-05-02 19:42:58 -0700

    Add teacher and assistance request tables

--- 3_queries_group-by/5_Low_Avg_Comp_Time.sql
+++ 3_queries_group-by/5_Low_Avg_Comp_Time.sql
@@ -3,3 +3,12 @@ FROM students
 JOIN assignment_submissions ON students.id = student_id
 JOIN assignments ON assignment_submissions.assignment_id = assignments.id
 GROUP BY student
+
+SELECT students.name as student, avg(assignment_submissions.duration) as average_assignment_duration, avg(assignments.duration) as average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments ON assignments.id = assignment_id
+WHERE end_date IS NULL
+GROUP BY student
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration;
